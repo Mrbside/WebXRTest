e7cdd6509ec0b3835ea1bcddca3bbf8fbd4b82dd
Budatron <braincandyweb@gmail.com>
2022-03-24 16:12:14 -0500

    add logs

--- Billetes/main.js
+++ Billetes/main.js
@@ -4,47 +4,48 @@ const THREE = window.MINDAR.IMAGE.THREE;
 document.addEventListener('DOMContentLoaded', () => {
   console.log('11111');
   const start = async() => {
-    console.log('24234dfsd');
+    console.log('22222');
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: 'billete.mind',
     });
+    console.log('3333');
     const {renderer, scene, camera} = mindarThree;
-
+    console.log('4444');
     const video = await loadVideo("billetevideo.mp4");
     const texture = new THREE.VideoTexture(video);
-
+    console.log('55555');
     const geometry = new THREE.PlaneGeometry(1, .67);
     const material = new THREE.MeshBasicMaterial({map: texture});
     const plane = new THREE.Mesh(geometry, material);
-
+    console.log('66666');
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(plane);
     var isplaying=false; 
-
+    console.log('77777');
     document.querySelector(".splash-bg").style.display = "none";
     document.querySelector(".splash-btn").style.display = "none";
     document.querySelector(".logo").style.display = "none";
    
-
-    video.addEventListener('pause', () => {
+    console.log('88888');
+    video.addEventListener('pause', () => { console.log('99999');
       if(isplaying){
        window.location.replace("https://www.auraxr.com/");}
     });
-    anchor.onTargetFound = () => {
+    anchor.onTargetFound = () => { console.log('10101010');
       isplaying=true;
       video.play();
     }
-    anchor.onTargetLost = () => {
+    anchor.onTargetLost = () => { console.log('once');
       isplaying=false;
       video.pause();
     }
-    video.addEventListener( 'play', () => {
+    video.addEventListener( 'play', () => { console.log('1212121');
       video.currentTime = 0;
     });
 
     await mindarThree.start();
-    renderer.setAnimationLoop(() => {
+    renderer.setAnimationLoop(() => { console.log('13131313');
       renderer.render(scene, camera);
     });
   }
